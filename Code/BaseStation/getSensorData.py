81685e62a5f98aaad9dade51775ab17d09cbe45c
RileyvanRyswyk <rileyvan@gmail.com>
2016-07-10 17:17:06 -0400

    redid getSensorData to extract soil measurement

--- Code/BaseStation/getSensorData.py
+++ Code/BaseStation/getSensorData.py
@@ -13,45 +13,17 @@ con = mdb.connect('localhost', 'aws_pi', 'T4hTvURJjm7xrhef', 'AWS');
 ser = serial.Serial('/dev/ttyAMA0', 115200, timeout=1)
 
 ser.reset_input_buffer()
-ser.write('AWS:2:S')
+ser.write('AWS:2:M')
 
-try:
-    serdata = ser.readline()
-    if len(serdata) > 3:
-        
-        # Clean the data. Strip any extra spaces.
-        nodemsgs = serdata.strip()
+for x in range(15):
+    time.sleep(1)
+    try:
+        serdata = ser.readline()
+        if len(serdata) > 3:
 
-        # Our data format uses comma to deliminate messages.
-        nodemsgs = nodemsgs.split(',')
-        nodemsgs = [x.strip('\x00') for x in nodemsgs]
-
-        for msg in nodemsgs:
-            # Our data format uses semicolon to deliminate data groups.
-            nodedata = msg.split(';')
-            nodedata = [x.strip('\x00') for x in nodedata]
-
-            if(nodedata[0] == 'AWS' and nodedata[2] == 'S'):
-                # Grab the NodeID from the message recieved
-                NodeID = nodedata[1]
-
-                for j in range(4,len(nodedata)-1):
-                    packet = nodedata[j].split(':')
-                    packet = [x.strip('\x00') for x in packet]
-
-                    charId = packet[0]
-                    data = packet[1]
-
-                    # Insert sensor node data into the database
-                    datasql = "INSERT INTO NodeData (nodeId, charId, data, received) VALUES ('{}','{}','{}',CURRENT_TIMESTAMP)".format (NodeID, charId, data)
-
-                    with con:
-                        cur = con.cursor()
-                        cur.execute(datasql)
-
-
-except Exception as inst:
-    print(type(inst))    # the exception instance
-    print(inst.args)     # arguments stored in .args
-    print(inst)          # __str__ allows args to be printed directly,
-    traceback.print_exc()
+            print serdata
+    except Exception as inst:
+        print(type(inst))    # the exception instance
+        print(inst.args)     # arguments stored in .args
+        print(inst)          # __str__ allows args to be printed directly,
+        traceback.print_exc()
--- Code/BaseStation/openValve.py
+++ Code/BaseStation/openValve.py
@@ -67,7 +67,7 @@ def getSensorData():
 
 schedule.every().day.at("5:00").do(job)
 schedule.every().day.at("6:55").do(job)
-schedule.every().minute.do(getSensorData)
+schedule.every(2).minutes.do(getSensorData)
 
 while True:
     schedule.run_pending()
