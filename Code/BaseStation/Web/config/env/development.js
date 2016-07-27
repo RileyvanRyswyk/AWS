7829a9ec0e7ab9a2c9e6f64f90ed277f4e90cdaf
RileyvanRyswyk <rileyvan@gmail.com>
2016-07-26 21:27:12 -0400

    Reorganized web files in BaseStation

--- Code/BaseStation/Web/config/env/development.js
+++ Code/BaseStation/Web/config/env/development.js
@@ -17,8 +17,11 @@ module.exports = {
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
 
-  // models: {
-  //   connection: 'someMongodbServer'
-  // }
+   models: {
+     connection: 'mysql'
+   },
+
+   // Because the PI is slow...
+   hookTimeout: 40000
 
 };
